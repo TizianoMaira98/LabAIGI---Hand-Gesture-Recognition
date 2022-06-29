49077f73f753d96a9cf6519c1634bae44e5aeb09
TizianoMaira98 <104787314+TizianoMaira98@users.noreply.github.com>
2022-06-29 19:40:29 +0200

    Hand Gesture Recognizer v1.0
    Program Rework:
    Having noticed that the program was not working to achieve the wanted type of result, also because the way it was written was biased by the use of a non proprietary dataset, both the algorithm and the dataset underwent profound changes:
    -Dataset used is now proprietary, and shall be enriched with more samples to have better training and test subsets
    -Comparison is now achieved through direct classification, starting with pre-labeled images, with the labels being the name of the dataset's subfolders
    -Data is kept in a list of couples (image_path,image_class)
    To Do;
    Testing section momentarily disabled and should be improved; Dataset should be reorganized to have unique images in the test subset

--- Riconoscitore_di_gesti_delle_mani.py
+++ Riconoscitore_di_gesti_delle_mani.py
@@ -5,183 +5,178 @@ import json
 import os
 from PIL import Image
 import matplotlib.pyplot as plt
+
 #DATA PATHS
-main_dataset_path="A_Very_Handy_Dataset"
-train_path="training"
-eval_path="evaluation"
-image_train_path=os.path.join(main_dataset_path,train_path,"rgb")
-image_eval_path=os.path.join(main_dataset_path,eval_path,"rgb")
-k_train_path=os.path.join(main_dataset_path,"training_K.json")
-labels_train_path=os.path.join(main_dataset_path,"training_xyz.json")
-#Some utilities function
-def projectPoints(xyz,K): #function that convert xyz point to uv point
-    xyz=np.array(xyz)
-    K=np.array(K)
-    uv=np.matmul(K,xyz.T).T
-    return [uv[0]/uv[2],uv[1]/uv[2]] #divide each element by third element to correctly project into image
-def load_file(file_name): #load file from json
-    with open(file_name,"r") as json_file: #"with" closes the opened file as soon as the operation in the with scope are all executed
-        data=json.load(json_file)
-    return data
-#Define dataset class
-class myDataset(torch.utils.data.Dataset):
-    train_split=25000
-    def __init__(self,image_path,K_path,labels_path,num_images,subset="train"):
-        self.image_path=image_path
-        self.subset=subset
-        self.num_images=num_images
-        if subset=="train":
-            #immagini in train non superiori a 25000
-            num_images=max(myDataset.train_split,num_images) # We NEED a more elegant way to do this...
-            self.images_names=np.sort(os.listdir(image_path))[:num_images]
-            self.Ks=np.array(load_file(K_path)[:num_images],dtype=np.float32) #need to specify the type as a 32 bit float, because...?
-            self.labels=np.array(load_file(labels_path)[:num_images],dtype=np.float32) #need to specify the type as a 32 bit float, because...?
-        elif subset=="eval":
-            #immagini in eval mai superiori a quelle rimaste
-            num_images=max(num_images,len(os.listdir(image_path))-myDataset.train_split)
-            self.images_names=np.sort(os.listdir(image_path))[myDataset.train_split:myDataset.train_split+num_images]
-            self.Ks=np.array(load_file(K_path),dtype=np.float32)[myDataset.train_split:myDataset.train_split+num_images]
-            self.labels=np.array(load_file(labels_path)[myDataset.train_split:myDataset.train_split+num_images],dtype=np.float32) #need to specify the type as a 32 bit float, because...?
-    def __getitem__(self,index): #given an index, return the corresponding image and the label
-        image_path=os.path.join(self.image_path,self.images_names[index])
-        image=np.array(Image.open(image_path),dtype=np.float32)
-        K=self.Ks[index]
-        labels=self.labels[index]
-        #labels must converted from xyz to uv
-        new_labels=[]
-        for i in range(len(labels)):
-            new_labels.append(projectPoints(labels[i],K)) #append new element to the array
-        new_labels=np.array(new_labels) #make array a np.array because its faster and more efficient
-        return image,new_labels
+main_dataset_path="Another_Very_Handy_Dataset"
+train_path="Training"
+test_path="Test"
+image_train_path=os.path.join(main_dataset_path,train_path)
+image_test_path=os.path.join(main_dataset_path,test_path)
+classes=os.listdir(image_train_path)
+
+# DEFINE DATASET CLASS
+class myDataset(torch.utils.data.Dataset):  
+    def __init__(self,image_path):
+        self.images_path=image_path
+        self.images_data=[] # list of couples (path,class)
+        classes=os.listdir(image_path) # list of all the classes
+        class_index=0 # index of the current class
+        # look into each subfolder
+        for class_ in classes:
+            current_class_images=os.listdir(os.path.join(self.images_path,class_)) # list of all the images path in the current class
+            for image in current_class_images:
+                complete_image_path=os.path.join(self.images_path,class_,image)
+                self.images_data.append([complete_image_path,class_index]) # append a new couple (image path,image class) to the list
+            class_index+=1 # next class of images
+    def __getitem__(self,index): # given an index, return the corresponding image and the label
+        image_path=self.images_data[index][0]
+        label=self.images_data[index][1]
+        im=Image.open(image_path)
+        im=im.resize((1280,720)) # resize image, so we can be sure they all have the same dims
+        image=np.array(im,dtype=np.float32) # dtype required cuz yes
+        image=image/255.0 #normalize image pixels to [0,1]
+        return image,label
     def __len__(self):
-        #return len(self.images_names)
-        return self.num_images #PERCHE' FATTA COSì (deep reason required)
-# MODELs DEFINITION
-#formula per ottenere le dimensioni in putput di un layer convoluzionale: [(size−Kernel+2*Padding)/Stride]+1
-class CNN(nn.Module):
-    def __init__(self):
-        super(CNN,self).__init__()
-        #extract features
-        self.conv1=nn.Conv2d(3,5,10)
-        self.conv2=nn.Conv2d(5,5,5)
-        self.conv3=nn.Conv2d(5,5,5)
-        self.conv4=nn.Conv2d(5,5,5)
-        self.pool=nn.MaxPool2d(2,2)
-        #get hand coordinates
-        self.fc1=nn.Linear(405,128)
-        self.fc3=nn.Linear(128,42)
-    def forward(self,im):
-        im=im.permute(0,3,1,2) #change order of channels
-        x=self.pool(torch.relu(self.conv1(im)))
-        x=self.pool(torch.relu(self.conv2(x)))
-        x=self.pool(torch.relu(self.conv3(x)))
-        x=self.pool(torch.relu(self.conv4(x)))
-        x=torch.flatten(x,1) #flatten output; needed because...?
-        x=torch.relu(self.fc1(x))
-        x=self.fc3(x)
-        return x
-#Alternative model: AlexNet
+        return len(self.images_data) 
+
+# MODEL DEFINITION
+# Chosen model: AlexNet
+    # formula per ottenere le dimensioni in output di un layer convoluzionale: [(size−Kernel+2*Padding)/Stride]+1
 class AlexNet(nn.Module):
     def __init__(self):
         super(AlexNet,self).__init__()
+        #estrae features dell'immagine
         self.features=nn.Sequential(
-            #1° conv
+            #N.B batch normalization added cuz seems to be good; param should/must be equal to he number of filters in Conv2d
+            #1° CONV
             nn.Conv2d(3,64,kernel_size=11,stride=4,padding=2),
+            nn.BatchNorm2d(64),
             nn.ReLU(inplace=True),
             nn.MaxPool2d(kernel_size=3,stride=2),
-            #2° conv
-            nn.Conv2d(64,192,kernel_size=5,padding=2),
+            #2° CONV
+            nn.Conv2d(64,128,kernel_size=5,padding=2),
+            nn.BatchNorm2d(128),
             nn.ReLU(inplace=True),
             nn.MaxPool2d(kernel_size=3,stride=2),
-            #3° conv
-            nn.Conv2d(192,384,kernel_size=3,padding=1),
+            #3° CONV
+            nn.Conv2d(128,64,kernel_size=3,padding=1),
+            nn.BatchNorm2d(64),
             nn.ReLU(inplace=True),
-            #4° conv
-            nn.Conv2d(384,256,kernel_size=3,padding=1),
-            nn.ReLU(inplace=True),
-            #5° conv
-            nn.Conv2d(256,256,kernel_size=3,padding=1),
+            #4° CONV
+            nn.Conv2d(64,32,kernel_size=3,padding=1),
+            nn.BatchNorm2d(32),
             nn.ReLU(inplace=True),
             nn.MaxPool2d(kernel_size=3,stride=2)
         )
+        #nel nostro caso non proprio classificatore ma comunque trasforma le feature nel dato che interessa EDIT: cioè?
         self.classifier=nn.Sequential(
             nn.Dropout(),
-            nn.Linear(256*6*6,4096),
+            nn.Linear(26208,1024),
             nn.ReLU(inplace=True),
             nn.Dropout(),
-            nn.Linear(4096,4096),
+            nn.Linear(1024,512),
             nn.ReLU(inplace=True),
-            nn.Linear(4096,42)
+            nn.Linear(512,5) 
         )
     def forward(self,im):
         x=self.features(im)
         x=torch.flatten(x,1)
         x=self.classifier(x)
         return x
+
+# TRAIN FUNCTION
+def train(epochs,model,optimizer,loss_func,train_loader="",test_loader="",device="cpu"):
+    train_losses=[]
+    train_acc=[]
+    test_losses=[]
+    for epoch in range(epochs):
+        correct_preds=0
+        print("\tEpoch:",epoch+1,"/",epochs)
+        for i,(image,labels) in enumerate(train_loader):
+            model.train()
+            image=image.to(device)
+            labels=labels.to(device)
+            image=image.permute(0,3,1,2) # change order of channels
+            outputs=model(image) # forward
+            # compute number of correct predictions
+            _,pred=torch.max(outputs,1) # get the index of the predicted classes, so the secon value raturned by max, discarding the first
+            correct=torch.sum(pred==labels) # compute number of correct predictions
+            correct_preds+=correct.item() # get number as normal python number
+            loss=loss_func(outputs,labels)# compute loss
+            # backward
+            optimizer.zero_grad()
+            loss.backward()
+            # update weights
+            optimizer.step()
+        #Save loss for successive plots
+        train_losses.append(loss.item())
+        accuracy=correct_preds/len(train_loader.dataset)
+        train_acc.append(accuracy)
+        print("\t\tLoss: {:.4f}  Accuracy: {:.3f}".format(loss.item(),accuracy))
+        torch.save(model.state_dict(),'model.pt')
+
+        '''
+        for i,(image,labels) in enumerate(test_loader):
+            #model switch to testuation mode, now dropout does not work
+            model.test()
+            image=image.to(device)
+            labels=labels.to(device)
+            image=image.permute(0,3,1,2) #change order of channels
+            outputs=model(image)
+            #compute loss
+            labels=torch.flatten(labels,start_dim=1)
+            loss=loss_func(outputs,labels)
+        
+        #Save the loss of this epoch to later plot the loss graph
+        #test_losses.append(loss.item())
+        #print("\t\ttest Loss: {:.4f}".format(loss.item()))
+        #Save the model with each epoch, so we can re-use it in successive trainings
+        '''
+    #torch.save(model.state_dict(),f'model_{epochs}_epochs.pt') #Save the weigths so we can re-use them without re-train the model
+    #plot losses in single image
+    train_losses=np.array(train_losses)
+    #test_losses=np.array(test_losses)
+    plt.clf()
+    plt.plot(train_losses,label="train_loss")
+    plt.legend() #add legend to the plot
+    plt.savefig(f"losses_{epochs}_epochs.png") #save the plot as png
+    plt.clf()
+    plt.plot(train_acc,label="train_acc")
+    plt.legend() #add legend to the plot
+    plt.savefig(f"accuracy_{epochs}_epochs.png") #save the plot as png
+    print("\tPlot completed")
+
 #----------------------MAIN----------------------
 print("\t_start_setup_menu_choices_placeholder_")
 # MODEL DECLARATION
-device=torch.device("cuda" if torch.cuda.is_available() else "cpu") #Use gpu if available
+device=torch.device("cuda" if torch.cuda.is_available() else "cpu") # Use gpu if available
+if device=="cuda": torch.cuda.empty_cache()
 print("\tActive device:",torch.cuda.get_device_name(0))
-#model=CNN().to(device)
 model=AlexNet().to(device)
 # Choosing loss e optimizer functions
-criterion=nn.MSELoss() #Mean Squared Error is general enough to work for us
-optimizer=torch.optim.Adam(model.parameters(),lr=1e-5) #Adam as optimizer sounds like a good idea
+loss_func=nn.CrossEntropyLoss() # This loss function seems to be more indicated when dealing with classification accuracy problems
+optimizer=torch.optim.Adam(model.parameters(),lr=1e-3) #Adam as optimizer sounds like a good idea
 print("\tModel created")
 # CREATE DATASET
-train_dataset=myDataset(image_train_path,k_train_path,labels_train_path,10000,subset="train") #create a dataset for training
+train_dataset=myDataset(image_train_path) #create a dataset for training
 print("\tTraining dataset created")
-eval_dataset=myDataset(image_train_path,k_train_path,labels_train_path,500,subset="eval") #create a dataset for testing
-print("\tEval dataset created")
+test_dataset=myDataset(image_train_path) #create a dataset for testing
+print("\ttest dataset created")
 # DATALOADER
 batch_size=16
 train_loader=torch.utils.data.DataLoader(train_dataset,batch_size=batch_size,shuffle=True)
-eval_loader=torch.utils.data.DataLoader(eval_dataset,batch_size=batch_size,shuffle=True)
-# TRAINING
-train_losses=[]
-eval_losses=[]
-epochs=22 # PaulCannon
-for epoch in range(epochs):
-    print("\tEpoch:",epoch+1,"/",epochs)
-    for i,(image,labels) in enumerate(train_loader):
-        model.train()
-        image=image.to(device)
-        labels=labels.to(device)
-        image=image.permute(0,3,1,2) ##change order of channels
-        outputs=model(image) #forward
-        #compute loss
-        labels=torch.flatten(labels,start_dim=1)
-        loss=criterion(outputs,labels)
-        #backward
-        optimizer.zero_grad()
-        loss.backward()
-        #update weights
-        optimizer.step()
-    #Save loss for successive plots
-    train_losses.append(loss.item())
-    print("\t\tTrain Loss: {:.2f}".format(loss.item()))
-    for i,(image,labels) in enumerate(eval_loader):
-        #model switch to evaluation mode, now dropout does not work
-        model.eval()
-        image=image.to(device)
-        labels=labels.to(device)
-        image=image.permute(0,3,1,2) #change order of channels
-        outputs=model(image)
-        #compute loss
-        labels=torch.flatten(labels,start_dim=1)
-        loss=criterion(outputs,labels)
-    #Save the loss of this epoch to later plot the loss graph
-    eval_losses.append(loss.item())
-    print("\t\tEval Loss: {:.2f}".format(loss.item()))
-    #Save the model with each epoch, so we can re-use it in successive trainings
-    torch.save(model.state_dict(),'model.pt')
-torch.save(model.state_dict(),f'model_{epochs}_epochs.pt') #Save the weigths so we can re-use them without re-train the model
-#plot losses in single image
-train_losses=np.array(train_losses)
-eval_losses=np.array(eval_losses)
-plt.plot(train_losses,label="train")
-plt.plot(eval_losses,label="eval")
-plt.legend() #add legend to the plot
-plt.savefig(f"losses_{epochs}_epochs.png") #save the plot as png
-print("\tPlot completed")
+test_loader=""
+#test_loader=torch.utils.data.DataLoader(test_dataset,batch_size=batch_size,shuffle=True)
+#TRAINING
+epochs=15
+train(epochs,model,optimizer,loss_func,train_loader,test_loader,device)
+
+'''
+#per caricare il modello dal file si usa:
+model=AlexNet().to(device) #initialize model
+model.load_state_dict(torch.load('model_15_epochs.pt')) #load weigths
+i="gino"
+while i!="stop":
+    i=input("inserisci numero immagine: ")
+    visualize_prediction(f"{'0'*(8-len(i))}{i}.jpg",model)
+'''
